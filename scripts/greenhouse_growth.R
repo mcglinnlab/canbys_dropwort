fc1a13f03b8548f75179ffff9f772d9df547111c
Dan McGlinn <danmcglinn@gmail.com>
2023-05-28 19:41:55 +0000

    update data and code after change to wide format

--- scripts/greenhouse_growth.R
+++ scripts/greenhouse_growth.R
@@ -7,66 +7,21 @@ head(dat)
 
 summary(dat)
 
-class(10)
-class("10")
 class(dat$tray)
 class(dat$status)
-table(dat$status)
 
-table(dat$date)
+# compute growth between 05/25 and 05/15
+gr_25 <- with(dat, len_0525 - len_0515)
+summary(gr_25)
+hist(gr_25)
 
-dat_09 <- subset(dat, date == '05/09/2023')
+# compute growth between 05/15 and 05/09
+gr_15 <- with(dat, len_0515 - len_0509)
+summary(gr_15)
+hist(gr_15)
 
-hist(dat_09$ht_cm)
-boxplot(ht_cm ~ tray, data = dat_09)
-boxplot(ht_cm ~ date, data = dat)
-
-t.test(ht_cm ~ date, data = dat)
-
-# no signal at population scale
-
-#let's look at individual level variation
-# drop any double measurements from single cell 
-head(dat)
-
-uni_id <- with(dat, 
-               paste(date, tray, column, row, sep='-'))
-duplicated(uni_id)
-dat_sub <- subset(dat, !duplicated(uni_id))
-# don't include date this time so that they can be compared
-uni_id <- with(dat_sub, 
-               paste(tray, column, row, sep='-'))
-table(uni_id)
-well_id <- unique(uni_id)
-
-ht_diff <- NULL
-for (i in seq_along(well_id)) {
-  ht_diff <- c(ht_diff, 
-               diff(dat_sub$ht_cm[uni_id == well_id[i]]))
-}
-ht_diff
-hist(ht_diff)
-
-well_id
-
-tray <- sapply(strsplit(well_id,'-'), function(x)x[[1]])
-
-boxplot(ht_diff ~ tray)
-
-
-y <- rnorm(100)
-x <- replicate(50, rnorm(100))
-
-dim(x)
-length(y)
-
-summary(lm(y ~ x))
-
-
-
-
-
-
+# examine rows in which gr15 growth was negative
+dat[which(gr_15 < 0), ]
 
 
 
