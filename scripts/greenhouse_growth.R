ec30b0e586143453b23eeadf6886a15f6cfa8340
Dan McGlinn <danmcglinn@gmail.com>
2023-05-30 16:18:05 +0000

    add comparison of size and growth

--- scripts/greenhouse_growth.R
+++ scripts/greenhouse_growth.R
@@ -15,6 +15,8 @@ gr_25 <- with(dat, len_0525 - len_0515)
 summary(gr_25)
 hist(gr_25)
 
+dat[which(gr_25 > 7), ]
+
 # compute growth between 05/15 and 05/09
 gr_15 <- with(dat, len_0515 - len_0509)
 summary(gr_15)
@@ -22,13 +24,27 @@ hist(gr_15)
 
 # examine rows in which gr15 growth was negative
 dat[which(gr_15 < 0), ]
+dat[which(gr_15 > 10), ]
 
 
+boxplot(gr_25 ~ dat$tray)
 
 
+summary(lm(gr_25 ~ as.factor(dat$tray)))
+car::Anova(lm(gr_25 ~ as.factor(dat$tray)))
 
+summary(lm(len_0525 ~ as.factor(tray), data = dat))
 
+dat$gr_25 <- gr_25
 
+plot(gr_25 ~ len_0525, data = dat)
+lines(lowess(dat$len_0525[!is.na(dat$len_0525)], gr_25[!is.na(dat$len_0525)]),
+      col='red', lwd =2)
+abline(lm(gr_25 ~ len_0525, data = dat), col='blue', lwd =2)
+summary(lm(gr_25 ~ len_0525, data = dat))
+points(gr_25 ~ len_0525, data = dat, subset = tray == 1, col='green3', pch = 19)
+points(gr_25 ~ len_0525, data = dat, subset = tray == 2, col='purple3', pch = 19)
+points(gr_25 ~ len_0525, data = dat, subset = tray == 3, col='dodgerblue', pch =19)
 
 
 
